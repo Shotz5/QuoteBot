beaf2811fce091d83f06fbb572e21636284f0b86
Alex Kitsul <kitsulalex@gmail.com>
2023-11-04 01:09:59 -0700

    Fixed word wrapping on boxes

--- Uploader/resources/js/Components/UI/Box.vue
+++ Uploader/resources/js/Components/UI/Box.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="border border-gray-200 dark:border-gray-800 rounded-md p-4 shadow-sm dark:text-gray-300 justify-center">
-        <div class="text-gray-400 font-medium mb-2 text-center">
+        <div class="text-gray-400 font-medium mb-2 text-center break-words">
             <slot name="header"></slot>
         </div>
         <slot></slot>
